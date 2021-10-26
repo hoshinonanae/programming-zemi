c8eeba419ec7e896f59c6a1129568f8b32bb4015
hoshinonanae <nanae.0805.mina@gmail.com>
2021-10-26 07:16:05 +0000

    basic signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get  'about' => "static_pages#about"
   get  'contact' => "static_pages#contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+get 'signup' =>'users#new'
+resources :users
 end
