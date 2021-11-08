8df5f532da13be37c7b59e01687d23bd9dc90f59
hoshinonanae <nanae.0805.mina@gmail.com>
2021-11-08 05:07:47 +0000

    basic signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,20 +5,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
-  def create
-    @user = User.new(user_params)  
-    if @user.save
-      # 保存の成功をここで扱う。
-      redirect_to @user
-    else
-      render 'new'
-    end
+end
+def create
+  @user = User.new(user_params)  
+  if @user.save
+    redirect_to @user
+  else
+    render 'new'
   end
+end
 
-  private
+private
 	
 	def user_params
-		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		params.require(:user).permit(:name, :email)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get  'signup' => "users#new"
   root 'static_pages#home'
+  get  'signup' => "users#new"
   get 'home' => "static_pages#home"
   get 'help' => "static_pages#help"
   get  'about' => "static_pages#about"
   get  'contact' => "static_pages#contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get 'signup' =>'users#new'
+get 'login'  =>'sessions#new'
+post 'login' =>'sessions#create'
+delete 'logout' =>'sessions#destroy'
 resources :users
 end
